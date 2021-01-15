e11e991be0e0acb27d16fd6214bfb2b81537b0fb
Nathan <nathaniel.mccaw@gmail.com>
2021-01-15 14:13:07 -0500

    added save button to text area

--- Develop/scripts.js
+++ Develop/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
+    //adding text element for time inside of the currentday div using Moment.js
     $("#currentDay").text(moment().format('LL'))
+
+    // listen for mouse click on save button
+
+    $(".saveBtn").on("click", function() {
+        // grab text inside of textArea
+        var userEntry = $(this).sibling(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // save intol local storage
+        localStorage.setItem(time, userEntry)
+    })
+
 });
