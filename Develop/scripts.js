3c2fe016ad4beeed221fb2e3dac40d59c0d6229b
Nathan <nathaniel.mccaw@gmail.com>
2021-01-15 14:28:35 -0500

    added colorizer

--- Develop/scripts.js
+++ Develop/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     //adding text element for time inside of the currentday div using Moment.js
-    $("#currentDay").text(moment().format('LL'))
+    $("#currentDay").text(moment().format("LL"));
 
     // listen for mouse click on save button
 
@@ -10,7 +10,29 @@ $(document).ready(function() {
         var time = $(this).parent().attr("id");
 
         // save intol local storage
-        localStorage.setItem(time, userEntry)
-    })
+        localStorage.setItem(time, userEntry);
+    });
 
+    function hourColorizer() {
+        //get the present number of hours
+        var presentHour = moment().hours();
+        $(".time-block").each(function() {
+            var hourBlock = parseInt($(this).attr("id").split("-")[0]);
+
+            //setting past, present and future
+
+            if (hourBlock < presentHour) {
+                $(this).addClass("past");
+            } else if (hourBlock === presentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+    }
+
+    hourColorizer();
 });
